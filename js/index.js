23c69db552a6561c952568a6150eea42f397d9c8
Kaise <tomasgonzalosanchez1@gmail.com>
2022-11-07 23:45:14 -0300

    Nice

---
+++ js/index.js
@@ -0,0 +1,18 @@
+document.querySelector(".bars__menu").addEventListener ("click",animateBars);
+document.querySelector(".bars__menu").addEventListener ("",animateBars);
+
+
+var line1__bars = document.querySelector(".line1__bars-menu");
+var line2__bars = document.querySelector(".line2__bars-menu");
+var line3__bars = document.querySelector(".line3__bars-menu");
+var list__ul = document.querySelector(".list-ul");
+
+function animateBars(){
+  line1__bars.classList.toggle("activeline1__bars-menu");
+  line2__bars.classList.toggle("activeline2__bars-menu");
+  line3__bars.classList.toggle("activeline3__bars-menu");
+  list__ul.classList.toggle("activehamburger__dropped")
+}
+
+
+
